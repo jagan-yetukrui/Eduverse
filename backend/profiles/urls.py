76db42491c408d400d386329d01acef6cb06a10c
vaishnavichennu <vaishnavi.chennu@gmail.com>
2024-12-30 17:52:18 -0500

    profile models created

--- backend/profiles/urls.py
+++ backend/profiles/urls.py
@@ -2,19 +2,27 @@ from django.urls import path
 from .views import UserProfileView
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ProfileViewSet
 
 from .views import WelcomeAPIView
 
 urlpatterns = [
-<<<<<<< HEAD
     path('', UserProfileView.as_view(), name='user_profile'),
 ]
 
 
 urlpatterns = [
     path('welcome/', WelcomeAPIView.as_view(), name='welcome'),
-=======
     path('admin/', admin.site.urls),
     path('profiles/', include('profiles.urls')),
->>>>>>> 548e78726cf4e1c3dfb4e14b89210eefa9301a97
+]
+
+
+
+router = DefaultRouter()
+router.register(r'profiles', ProfileViewSet, basename='profile')
+
+urlpatterns = [
+    path('', include(router.urls)),
 ]
