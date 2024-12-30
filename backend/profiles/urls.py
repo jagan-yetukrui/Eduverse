548e78726cf4e1c3dfb4e14b89210eefa9301a97
Juhun Park <riann3207@gmail.com>
2024-12-30 14:20:03 -0500

    test profiles api

--- backend/profiles/urls.py
+++ backend/profiles/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import UserProfileView
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('', UserProfileView.as_view(), name='user_profile'),
+    path('admin/', admin.site.urls),
+    path('profiles/', include('profiles.urls')),
 ]
