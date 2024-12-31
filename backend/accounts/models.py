3ed10aa84606cdbf1ea44a34844d79948aea4308
Juhun Park <riann3207@gmail.com>
2024-12-30 19:18:59 -0500

    Add comment inherited fields in CustomUser model

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class CustomUser(AbstractUser):
     profile_picture = models.ImageField(upload_to='profiles/', blank=True, null=True)
     skills = models.TextField(blank=True, null=True)
+    # username, password, email, first_name, last_name are inherited from AbstractUser
 
     def __str__(self):
         return self.username
