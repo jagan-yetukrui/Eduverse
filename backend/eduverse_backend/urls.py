e303b001089178d4f07e7d332dc8b10efa7291cc
Aakash Tripathi <aakash7tripathi@gmail.com>
2025-01-17 14:13:23 -0500

    Made changes to settings.py

--- backend/eduverse_backend/urls.py
+++ backend/eduverse_backend/urls.py
@@ -18,6 +18,7 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path, include
 from django.views.generic import TemplateView
 from rest_framework_simplejwt.views import (
@@ -26,6 +27,7 @@ from rest_framework_simplejwt.views import (
 )
 
 urlpatterns = [
+    path('', lambda request: HttpResponse("Welcome to Eduverse!")),
     path("admin/", admin.site.urls),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
