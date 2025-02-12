6b7c54afe459905b66744f42964fffb446122a65
Acash2018 <aakash7tripathi@gmail.com>
2025-02-11 20:15:15 -0500

    from django.contrib.auth.models import User was added in models.py

--- newbackend/accounts/models.py
+++ newbackend/accounts/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 # from django.contrib.postgres.fields import ArrayField
+from django.contrib.auth.models import User
 
 
 class CustomUser(AbstractUser):
