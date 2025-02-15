6f339c10ca8dae0e398c959a2614608245ab592c
Acash2018 <aakash7tripathi@gmail.com>
2025-02-15 18:58:00 -0500

    updated views.py

--- backend/ai/views.py
+++ backend/ai/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .serializers import PostSerializer
+from .models import Post
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+class PostListCreateView(APIView):
+    def get(self, request):
+        posts = Post.objects.all()
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
