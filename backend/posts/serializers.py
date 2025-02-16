f001898e352c7ef04a10c67a1ea835768165a4b0
Acash2018 <aakash7tripathi@gmail.com>
2025-02-15 19:04:40 -0500

    Indentation on serializers.py

--- backend/posts/serializers.py
+++ backend/posts/serializers.py
@@ -5,6 +5,7 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = ['id', 'title', 'content', 'created_at']
+        
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
