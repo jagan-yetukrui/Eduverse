9235f4049a3751631620bc5609953e75ae05a183
Acash2018 <aakash7tripathi@gmail.com>
2025-02-15 19:07:19 -0500

    Url.py has routes for both posts and comments.

--- backend/urls.py
+++ backend/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('api/profiles/me', ProfileView.as_view(), name='profile_me'),
     path('api/profiles/<str:user_id>', ProfileView.as_view(), name='profile_detail'),
     path('api/profiles/', include('profiles.urls')),
-      path('api/posts/', include('posts.urls')),
+    path('api/posts/', include('posts.urls')),
+    path('posts/', PostListCreateView.as_view(), name='post-list-create'),
+    path('comments/', CommentListView.as_view(), name='comment-list-create'),
 ] 
