94a3239daf9f7f4bab060483a60940023d08e04d
Acash2018 <aakash7tripathi@gmail.com>
2025-02-15 19:02:40 -0500

    Created a comment serializer

--- backend/posts/serializers.py
+++ backend/posts/serializers.py
@@ -5,3 +5,7 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = ['id', 'title', 'content', 'created_at']
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['id', 'post', 'text', 'created_at']
