7f55b4fb4aaad2083ee7587e930ff3a6a90d95db
Acash2018 <aakash7tripathi@gmail.com>
2025-02-15 19:41:24 -0500

    Adding Save on the backend

--- backend/posts/serializers.py
+++ backend/posts/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Post, Comment, Like
+from .models import Post, Comment, Like, Save
 
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
@@ -15,3 +15,8 @@ class LikeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Like
         fields = ['id', 'post', 'user', 'created_at']  # Add fields as per your `Like` model
+
+class SaveSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Save
+        fields = ['id', 'user', 'post', 'created_at']  # Add fields relevant to your Save model
