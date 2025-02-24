57f1d4308410e26c7802c180fddb2805ab60a101
Acash2018 <aakash7tripathi@gmail.com>
2025-02-24 17:51:59 -0500

    Updated access and refresh token lifetimes

--- backend/urls.py
+++ backend/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('accounts.urls')),  # ✅ Ensures /api/login/ works
+    path('api/token', include('accounts.urls')),  # ✅ Ensures /api/login/ works
     path('api/posts/', include('posts.urls')),
 ]
