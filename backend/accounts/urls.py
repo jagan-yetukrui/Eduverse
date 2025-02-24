ae5108030b3109ee1f522c4f46078fc925a30b78
Acash2018 <aakash7tripathi@gmail.com>
2025-02-24 14:22:33 -0500

    added the login api to the log in page.

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from .views import AccountsRootView, RegisterView, LoginView, ProtectedView
-from rest_framework_simplejwt.views import TokenRefreshView
+from rest_framework_simplejwt.views import TokenRefreshView,  TokenObtainPairView
 
 urlpatterns = [
     path('', AccountsRootView.as_view(), name='accounts-root'),  # Root of `/api/accounts/`
     path('register/', RegisterView.as_view(), name='register'),
-    path('login/', LoginView.as_view(), name='login'),
+     path('login/', TokenObtainPairView.as_view(), name='login'),  # ✅ Ensures login works
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('protected-endpoint/', ProtectedView.as_view(), name='protected-endpoint'),
 ]
--- backend/urls.py
+++ backend/urls.py
@@ -4,5 +4,6 @@ from profiles.views import ProfileView
 urlpatterns = [
     path('api/profiles/me', ProfileView.as_view(), name='profile_me'),
     path('api/profiles/<str:user_id>', ProfileView.as_view(), name='profile_detail'),
+    path('api/', include('accounts.urls')),  # ✅ Ensures /api/login/ works
     path('api/profiles/', include('profiles.urls')),
 ] 
