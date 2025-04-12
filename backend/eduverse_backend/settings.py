d96ebfeae739f5af8dd5bdf1d6e77774232a34ca
Acash2018 <aakash7tripathi@gmail.com>
2025-04-12 19:51:51 -0400

    `Update .env and settings.py for PostgreSQL database configuration`

--- backend/eduverse_backend/settings.py
+++ backend/eduverse_backend/settings.py
@@ -99,9 +99,13 @@ WSGI_APPLICATION = 'eduverse_backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+  'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'Jagan',
+        'PASSWORD': 'TeamStrongPassword123!',
+        'HOST': 'eduverse-db.crgqasis8mt9.us-east-2.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
